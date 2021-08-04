6ba43f3e03f699eaa99b83e9cc8fa57b940db692
makil2 <silvapinheirojoao@gmail.com>
2021-08-04 11:40:30 -0300

    Obtendo um post por id

---
+++ src/main/java/com/joaopaulo/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.joaopaulo.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaopaulo.workshopmongo.domain.Post;
+import com.joaopaulo.workshopmongo.repository.PostRepository;
+import com.joaopaulo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Post user = repo.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+}
